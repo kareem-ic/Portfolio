1ab2bd121540afc465a763e4a73e5b09ad889eac
kareem-ic <kclarke11506@gmail.com>
2025-07-06 16:05:07 -0400

    Updated homepage designs and animations

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,24 +1,31 @@
 import LogoTitle from '../../assets/images/K.png';
 import { Link } from 'react-router-dom';
-
+import './index.scss';
 
 const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi,
+          <br /> 
+          I'm
+          <span className="logo-letter">
+            <img src={LogoTitle} alt="developer" />
+          </span>
+          areem,
+          <br />
+          software engineer
+        </h1>
 
+        <h2>FullStack Developer / AI Expert</h2>
 
-    return (
-        <div className="container home-page">
-            <div className='text-zone'>
-                <h1>Hi, <br /> I'm
-                <img src={LogoTitle} alt="developer" />
-                areem
-                <br />
-                web developer
-                </h1>
-                <h2>FullStack Developer / AI Expert </h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-            </div>
-        </div>
-    );
-}
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
