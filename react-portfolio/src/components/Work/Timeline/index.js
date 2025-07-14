7ed8517bb20711dc4a5aae0a0d7c9f4fdc789560
kareem-ic <kclarke11506@gmail.com>
2025-07-13 23:07:00 -0400

    Next work experience updated

--- react-portfolio/src/components/Work/Timeline/index.js
+++ react-portfolio/src/components/Work/Timeline/index.js
@@ -3,6 +3,17 @@ import './index.scss';
 
 const Timeline = () => {
   const [events, setEvents] = useState([
+    
+    {
+      date: 'Dec 2024 – Feb 2025',
+      title: 'Software Engineer Intern — Toyz Electronics, Inc.',
+      description: [
+        'Redesigned the site’s architecture in <b>React</b> to address usability gaps, leading to a smoother user flow for 4.4K+ monthly users and reducing navigation drop-offs across key pages',
+        'Modernized key UI components to improve visual hierarchy and accessibility, aligning the design with WCAG guidelines and enhancing clarity for both mobile and desktop users',
+        'Orchestrated cross-functional collaboration with design and product teams to revamp UX elements, contributing to a <b>22%</b> increase in time-on-site and improved interaction consistency',
+      ],
+      expanded: false,
+    },
     {
       date: 'May 2024 – Sep 2024',
       title: 'Software Engineer Intern — Soapdocs LLC',
